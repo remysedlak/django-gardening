956b5d7e59502c1d770e1fdd22e7333d66add5ea
remysedlak <remysedlak@gmail.com>
2025-05-13 08:23:55 -0400

    updated views for admin site. Plant, image, and note entry

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Plant, PlantImage, Note
 
 # Register your models here.
+admin.site.register(Plant)
+admin.site.register(PlantImage)
+admin.site.register(Note)
