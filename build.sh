d873b5255c06a8e54fd2592fb519dfc637233761
remysedlak <remysedlak@gmail.com>
2025-05-13 07:29:01 -0400

    fix: correct set command and remove unnecessary ls and cd commands in build script

--- build.sh
+++ build.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 # Exit on error
-set -o errexit
-
-ls -a
+set -o errexi
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
-
-ls -a
-
-cd backend
 
 # Convert static asset files
 python manage.py collectstatic --no-input
