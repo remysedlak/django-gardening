ab6865923f7a231fcf035a99ef03c382fb102706
remysedlak <remysedlak@gmail.com>
2025-05-13 07:56:16 -0400

    creating admin for render

--- backend/build.sh
+++ backend/build.sh
@@ -10,3 +10,11 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+# Create superuser if it doesn't exist
+echo "
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'yourpassword')
+" | python manage.py shell
