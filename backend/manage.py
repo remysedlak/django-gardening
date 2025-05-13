2e6f8f217981c6fd8c5cc6b52850a39b4879643a
remysedlak <remysedlak@gmail.com>
2025-05-12 22:35:09 -0400

    fix: api and base both not in base.

--- backend/manage.py
+++ backend/manage.py
@@ -3,6 +3,12 @@
 import os
 import sys
 
+# Add the outer "backend/" directory to sys.path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+from django.core.management import execute_from_command_line
+execute_from_command_line(sys.argv)
 
 def main():
     """Run administrative tasks."""
